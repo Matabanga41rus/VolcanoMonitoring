8bd8ea127e51822ca0d6ed863171b7d189bad774
matabanga41rus <merkushev9941@gmail.com>
2020-12-01 00:36:08 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import xlrd
+
+
+
+
+
+
+pathExcel = 'C:/Users/Denis/Desktop/excel_python/26_Dlz.xlsx'
+
+
+
+def getDataExcel(pathExcel):
+    rb = xlrd.open_workbook(pathExcel)
+    sheet = rb.sheet_by_index(0)
+
+    date = sheet.col_values(2)
+    time = sheet.col_values(3)
+    station = sheet.col_values(4)
+    volcano = sheet.col_values(6)
+    countEvent = sheet.col_values(8)
+
+    return 0
