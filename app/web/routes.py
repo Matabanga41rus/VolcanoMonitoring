620fce702fcf9507875929b75029418124e675c5
matabanga41rus <merkushev9941@gmail.com>
2021-02-03 00:12:45 +1200

    created condition checking authenticated on index.html

--- app/web/routes.py
+++ app/web/routes.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
-from app import app
-from app.web.forms import SeismicObservationForm, ObservationForm
-from app.web.models import SeismicObservation, Observation
-
+from flask import render_template, redirect, url_for, flash
+from datetime import datetime
+from app import app, db
+from app.web.forms import SeismicObservationForm, LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.web.models import SeismicObservation, EventType, Station, Observation, Operator, Volcano
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    if current_user.is_authenticated:
+        return render_template('index.html')
+
     return render_template('index.html')
 
 @app.route('/add')
