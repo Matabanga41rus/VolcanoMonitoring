5805b94056b7956f0bddaa989534a38aaf08d0a1
matabanga41rus <merkushev9941@gmail.com>
2021-02-15 09:58:28 +1200

    Created func for check and add Observation

--- app/web/models.py
+++ app/web/models.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass
 from sqlalchemy import Column, BIGINT, Integer, String, SMALLINT, REAL, DATE, Boolean, ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 from app import login
 from app import db
 
+
 @dataclass()
 class Volcano(db.Model):
-    __tablename__='Volcano'
+    __tablename__ = 'Volcano'
     volcId = Column(BIGINT, primary_key=True)
     volcName = Column(String)
     volcNameLat = Column(String)
@@ -19,81 +21,83 @@ class Volcano(db.Model):
 @dataclass()
 class VolcanoStation(db.Model):
     __tablename__ = "VolcanoStation"
-    volcstId= Column(Integer, primary_key=True)
-    volcstVolcanoId= Column(Integer, ForeignKey('Volcano.volcId'))
-    volcstStationId= Column(Integer, ForeignKey('Station.stId'))
-    volcstDistance= Column(REAL)
-    volcstPreffered= Column(Boolean)
+    volcstId = Column(Integer, primary_key=True)
+    volcstVolcanoId = Column(Integer, ForeignKey('Volcano.volcId'))
+    volcstStationId = Column(Integer, ForeignKey('Station.stId'))
+    volcstDistance = Column(REAL)
+    volcstPreffered = Column(Boolean)
 
 
 @dataclass()
 class Station(db.Model):
-    __tablename__='Station'
-    stId= Column(Integer, primary_key=True)
-    stName= Column(String)
-    stNameLat= Column(String)
-    stRegionalCode= Column(String)
-    stInternationalCode= Column(String)
-    stInternationalCodeRegistered= Column(DATE)
-    stComment= Column(String)
-    stState= Column(Integer)
+    __tablename__ = 'Station'
+    stId = Column(Integer, primary_key=True)
+    stName = Column(String)
+    stNameLat = Column(String)
+    stRegionalCode = Column(String)
+    stInternationalCode = Column(String)
+    stInternationalCodeRegistered = Column(DATE)
+    stComment = Column(String)
+    stState = Column(Integer)
 
 
 @dataclass()
 class Network(db.Model):
     __tablename__ = "Network"
-    netId= Column(Integer, primary_key=True)
-    netName= Column(String)
-    netRemId= Column(Integer)
-    netNameLat= Column(String)
-    netCode= Column(String)
+    netId = Column(Integer, primary_key=True)
+    netName = Column(String)
+    netRemId = Column(Integer)
+    netNameLat = Column(String)
+    netCode = Column(String)
 
 
 @dataclass()
 class Instrument(db.Model):
     __tablename__ = "Instrument"
-    instId= Column(Integer, primary_key=True)
-    instNetId= Column(Integer, ForeignKey('NetworkId'))
-    instStId= Column(Integer, ForeignKey('Station.stId'))
-    instLocation= Column(Integer)
-    instName= Column(String)
-    instOrgId= Column(Integer, ForeignKey(''))
-    instOrder= Column(Integer)
+    instId = Column(Integer, primary_key=True)
+    instNetId = Column(Integer, ForeignKey('NetworkId'))
+    instStId = Column(Integer, ForeignKey('Station.stId'))
+    instLocation = Column(Integer)
+    instName = Column(String)
+    instOrgId = Column(Integer, ForeignKey(''))
+    instOrder = Column(Integer)
 
 
 @dataclass()
 class HypoCenter(db.Model):
     __tablename__ = "HypoCenter"
-    hypId= Column(Integer, primary_key=True)
+    hypId = Column(Integer, primary_key=True)
 
 
 @dataclass()
 class EventType(db.Model):
     __tablename__ = 'EventType'
-    typeId= Column(Integer, primary_key=True)
-    type= Column(String)
+    typeId = Column(Integer, primary_key=True)
+    type = Column(String)
 
 
 @dataclass()
 class NoteVideoObs(db.Model):
     __tablename__ = "NoteVideoObs"
-    nvoId= Column(Integer, primary_key=True)
-    nvoNote= Column(String)
-    nvoRelevanceNote= Column(Boolean)
+    nvoId = Column(Integer, primary_key=True)
+    nvoNote = Column(String)
+    nvoRelevanceNote = Column(Boolean)
+    nvoOperatorId = Column(Integer, ForeignKey('Operator.opId'))
+    nvoDateSave = Column(DATE)
 
 
 @dataclass()
 class Camera(db.Model):
     __tablename__ = "Camera"
-    cmId= Column(Integer, primary_key=True)
-    cmName= Column(String)
+    cmId = Column(Integer, primary_key=True)
+    cmName = Column(String)
 
 
 class Operator(db.Model, UserMixin):
-    __tablename__='Operator'
-    opId= Column(Integer, primary_key=True)
-    opSurname= Column(String)
-    opPasswordHash= Column(String)
+    __tablename__ = 'Operator'
+    opId = Column(Integer, primary_key=True)
+    opSurname = Column(String)
+    opPasswordHash = Column(String)
 
     def get_id(self):
         return self.opId
@@ -104,6 +108,7 @@ class Operator(db.Model, UserMixin):
     def checkPassword(self, password):
         return check_password_hash(self.opPasswordHash, password)
 
+
 @login.user_loader
 def load_user(id):
     return Operator.query.get(int(id))
@@ -113,50 +118,77 @@ def load_user(id):
 @dataclass()
 class Observation(db.Model):
     __tablename__ = "Observation"
-    obsId= Column(Integer, primary_key=True)
-    obsDate= Column(DATE)
-    obsVolcanoId= Column(Integer, ForeignKey('Volcano.volcId'))
-    obsOperatorId= Column(Integer, ForeignKey('Operator.opId'))
+    obsId = Column(Integer, primary_key=True)
+    obsDate = Column(DATE)
+    obsVolcanoId = Column(Integer, ForeignKey('Volcano.volcId'))
+    obsOperatorId = Column(Integer, ForeignKey('Operator.opId'))
+    obsCreatedBy = Column(String)
+    obsDateSave = Column(DATE)
+
+    def is_check_data_added_today(self, opId, date, volcId):
+        obs = Observation()
+
+        if obs.query.filter_by(obsOpId=opId, obsDate=date, obsVolcanoId=volcId).first() is None:
+            return False
+        else:
+            return True
+
+    def added_observation(self, opId, date, volcId, createdBy):
+        dateServer = datetime.now()
+        obs = Observation(obsDate=date,
+                          obsVolcanoId=volcId,
+                          obsOperatorId=opId,
+                          obsCreatedBy=createdBy,
+                          obsDateSave=dateServer)
+        try:
+            db.session.add(obs)
+            db.session.commit()
+            observation = obs  # для дальнейшего заполнения seisobs
+        except:
+            print('error database')
 
 
 @dataclass()
 class VideoObservation(db.Model):
     __tablename__ = "VideoObservation"
-    vobsId= Column(Integer, primary_key=True)
-    vobsObservationId= Column(Integer, ForeignKey(''))
-    vobsHeightDischarge= Column(SMALLINT)
-    vobsNvoId= Column(Integer, ForeignKey('NoteVideoObs,nvoId'))
-    vobsCmId= Column(Integer, ForeignKey('Camera.cmId'))
+    vobsId = Column(Integer, primary_key=True)
+    vobsObservationId = Column(Integer, ForeignKey(''))
+    vobsHeightDischarge = Column(SMALLINT)
+    vobsNvoId = Column(Integer, ForeignKey('NoteVideoObs,nvoId'))
+    vobsCmId = Column(Integer, ForeignKey('Camera.cmId'))
+    vobsOperatorId = Column(Integer, ForeignKey('Operator.opId'))
+    vobsDateSave = Column(DATE)
 
 
 @dataclass()
 class SatelliteObservation(db.Model):
     __tablename__ = "SatelliteObservation"
-    satobsId= Column(Integer, primary_key=True)
-    satobsObservationId= Column(Integer, ForeignKey('Observation.obsId'))
-    satobsPixels= Column(SMALLINT)
-    satobsTmax= Column(SMALLINT)
-    satobsTfon= Column(SMALLINT)
+    satobsId = Column(Integer, primary_key=True)
+    satobsObservationId = Column(Integer, ForeignKey('Observation.obsId'))
+    satobsPixels = Column(SMALLINT)
+    satobsTmax = Column(SMALLINT)
+    satobsTfon = Column(SMALLINT)
+    satobsOperatorId = Column(Integer, ForeignKey('Operator.opId'))
 
 
 @dataclass()
 class SeismicObservation(db.Model):
-    __tablename__="SeismicObservation"
-    seisobsId= Column(Integer, primary_key=True)
-    seisobsObservationId= Column(Integer, ForeignKey('Observation.obsId'))
-    seisobsStationId= Column(Integer, ForeignKey('Station.stId'))
-    seisobsInstrumentId= Column(Integer, ForeignKey('Instrument.instId'))
-    seisobsOperatorId= Column(Integer, ForeignKey('Operator.opId'))
-    seisobsStartTime= Column(DATE)
-    seisobsEndTime= Column(DATE)
+    __tablename__ = "SeismicObservation"
+    seisobsId = Column(Integer, primary_key=True)
+    seisobsObservationId = Column(Integer, ForeignKey('Observation.obsId'))
+    seisobsStationId = Column(Integer, ForeignKey('Station.stId'))
+    seisobsInstrumentId = Column(Integer, ForeignKey('Instrument.instId'))
+    seisobsOperatorId = Column(Integer, ForeignKey('Operator.opId'))
+    seisobsStartTime = Column(DATE)
+    seisobsEndTime = Column(DATE)
     seisobsPeriodStartTime = Column(DATE)
     seisobsPeriodEndTime = Column(DATE)
-    seisobsHypocenterId= Column(Integer, ForeignKey('HypoCenter.hypId'))
-    seisobsEventCount= Column(SMALLINT)
+    seisobsHypocenterId = Column(Integer, ForeignKey('HypoCenter.hypId'))
+    seisobsEventCount = Column(SMALLINT)
     seisobsWeak = Column(Boolean)
-    seisobsEventTypeId= Column(Integer, ForeignKey('EventType.typeId'))
-    seisobsAvgAT= Column(REAL)
-    seisobsMaxAT= Column(REAL)
-    seisobsDuration= Column(SMALLINT)
-    seisobsEnergyClass= Column(REAL)
-    seisobsDateSave= Column(DATE)
+    seisobsEventTypeId = Column(Integer, ForeignKey('EventType.typeId'))
+    seisobsAvgAT = Column(REAL)
+    seisobsMaxAT = Column(REAL)
+    seisobsDuration = Column(SMALLINT)
+    seisobsEnergyClass = Column(REAL)
+    seisobsDateSave = Column(DATE)
